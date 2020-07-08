b0cdc1aa1f558120e8c652886be97ebf7e670411
pkp09 <pandeyprashant@live.in>
2020-07-08 11:27:15 +0530

    ELK Stack Implentation Added

---
+++ src/main/java/com/prashant/microservice/elkstack/ElkStackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.microservice.elkstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkStackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkStackExampleApplication.class, args);
+	}
+
+}
